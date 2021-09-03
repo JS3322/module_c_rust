6815875d1dae233c8a360732d9e1b5ca2f1123ba
js <bada@ganada.kr>
2021-09-03 11:03:38 +0900

    init hello world

---
+++ target/debug/deps/untitled.d
@@ -0,0 +1,5 @@
+/Users/js/development/unit_rust/untitled/target/debug/deps/untitled: src/main.rs
+
+/Users/js/development/unit_rust/untitled/target/debug/deps/untitled.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/untitled.d
@@ -0,0 +1,1 @@
+/Users/js/development/unit_rust/untitled/target/debug/untitled: /Users/js/development/unit_rust/untitled/src/main.rs
